206f5dc7694d95a0ffa318f8e4a58d35f361c62b
xgqfrms <xgqfrms@outlook.com>
2022-10-25 23:40:06 +0800

    feat: sed 同时执行多个命令 `-e`, 使用英文的分号分隔 `;` ✅

--- 000-xyz/Linux-Bash-Commands/sed-replace.sh
+++ 000-xyz/Linux-Bash-Commands/sed-replace.sh
@@ -11,7 +11,24 @@ echo -e "sed starting"
 # sed "s/div/XYZ" ./multi-line-text.txt
 
 # /regexp/ 正则表达式后面少了一个 `/` ✅
-sed "s/div/XYZ/" ./multi-line-text.txt >> multi-line-text.md
+# sed "s/div/XYZ/" ./multi-line-text.txt >> multi-line-text.md
+# sed "s/<div>//" | sed "s/</div>//" ./multi-line-text.txt >> multi-line-text.md
+# sed "s/<div>//" "s/</div>//" ./multi-line-text.txt >> multi-line-text.md
+# sed "s/<div><\/div>//" ./multi-line-text.txt >> multi-line-text.md
+# sed "s/<div>//" | sed "s/<\/div>//" ./multi-line-text.txt >> multi-line-text.md
+
+# ??? 正则 replace multi tags
+
+# sed 不会直接修改源文件, 只是返回一个 STDOUT ✅
+# sed "s/<div>/👻/" ./multi-line-text.txt
+
+# sed 同时执行多个命令 `-e`, 使用英文的分号分隔 `;` ✅
+# sed 的多个命令写在同一个双引号里面
+# sed -e "s/<div>//; s/<\/div>//" ./multi-line-text.txt >> multi-line-text.md
+sed -e "s/<div>/🎉/; s/<\/div>/👻/" ./multi-line-text.txt >> multi-line-text.md
+
+# sed -e "s/<div>//"; "s/<\/div>//" ./multi-line-text.txt >> multi-line-text.md
+
 
 # echo ./multi-line-text.txt
 # echo ./multi-line-text.txt | sed "s/div/XYZ"
